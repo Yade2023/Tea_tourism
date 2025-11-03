87c7d6aac09f24ff6046f44a55eec7ad690036ce
Yade2023 <144205116+Yade2023@users.noreply.github.com>
2025-11-03 17:09:46 +0800

    1
    1

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,45 +5,43 @@ import '../assets/css/Footer.css';
 <template>
   <footer class="footer">
     <div class="footer-container">
-      <!-- 左側區塊 -->
+      
+      <!-- 品牌資訊 -->
       <div class="footer-column">
-        <h4>茶葉網站</h4>
-        <p>服務時間：平日，09:00-17:00</p>
-        <p>LINE：@teaway</p>
+        <h4>茶旅</h4>
+        <p>版權所有 &copy; 2023 茶旅</p>
       </div>
 
-      <!-- 首頁 -->
+      <!-- 網站導覽 -->
       <div class="footer-column">
         <h4><a href="/#">首頁</a></h4>
       </div>
 
-      <!-- 台灣茶知識庫 -->
       <div class="footer-column">
         <h4><a href="/knowledge">台灣茶知識庫</a></h4>
       </div>
 
-      <!-- 茶旅專區 -->
       <div class="footer-column">
         <h4><a href="/teaTourismArea">茶旅專區</a></h4>
       </div>
 
-      <!-- 線上茶店 -->
       <div class="footer-column">
         <h4><a href="/">線上茶店</a></h4>
       </div>
 
-      <!-- 關於我們 -->
+      <!-- 服務資訊 -->
       <div class="footer-column">
-       <h4>關於我們</h4>
+        <h4>聯絡我們</h4>
         <ul>
-          <li><a href="/address">聯絡表單</a></li>
+          <li><a href="/address">聯絡我們</a></li>
         </ul>
       </div>
 
-      <!-- 登入註冊 -->
+      <!-- 用戶功能 -->
       <div class="footer-column">
         <h4><a href="/login">登入、註冊</a></h4>
       </div>
+
     </div>
   </footer>
 </template>
